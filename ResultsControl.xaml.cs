0005bc3e46c6737a9cebc41ef2fc2ed06432b84d
MargoShestakova <margo.shestakova.06@bk.ru>
2025-04-27 15:38:03 +0300

    Add files via upload

---
+++ ResultsControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Controls;
+
+namespace SimptomChek
+{
+    public partial class ResultsControl : UserControl
+    {
+        public ResultsControl()
+        {
+            InitializeComponent();
+        }
+
+        public string ResultsText
+        {
+            get { return ResultsTextBox.Text; }
+            set { ResultsTextBox.Text = value; }
+        }
+    }
+}
